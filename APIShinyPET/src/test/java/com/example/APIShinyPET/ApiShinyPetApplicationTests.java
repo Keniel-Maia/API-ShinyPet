6f62603e56e82bb089f511e89236ad2bc2af43c7
Keniel <keniellms@gmail.com>
2025-10-23 22:13:14 -0300

    Enable asynchronous processing in tests

--- APIShinyPET/src/test/java/com/example/APIShinyPET/ApiShinyPetApplicationTests.java
+++ APIShinyPET/src/test/java/com/example/APIShinyPET/ApiShinyPetApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.APIShinyPET;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootTest
 class ApiShinyPetApplicationTests {
 
