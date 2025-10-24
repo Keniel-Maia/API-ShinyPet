3ae2dfd58cffea5fecfab22e550350a2c06d5ef8
Keniel <keniellms@gmail.com>
2025-10-23 22:10:22 -0300

    Make enviarEmailCadastro method asynchronous
    Added asynchronous processing to the enviarEmailCadastro method.

--- APIShinyPET/src/main/java/com/example/APIShinyPET/service/EmailService.java
+++ APIShinyPET/src/main/java/com/example/APIShinyPET/service/EmailService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.scheduling.annotation.Async;
 
 @Service
 public class EmailService {
@@ -12,6 +13,7 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Async
     public void enviarEmailCadastro(String nomeCompleto, String email) {
         try {
             MimeMessage mensagem = mailSender.createMimeMessage();
